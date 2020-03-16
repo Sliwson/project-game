44b20c12519e883db8150af08b4ef125b4956076
Mateusz Śliwakowski <mateusz_sliw@interia.pl>
2020-03-16 17:28:32 +0100

    Add incomplete helper classes #IO-22

---
+++ TheProjectGame/GameMaster/Field.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameMaster
+{
+    public class Field
+    {
+        public FieldState State { get; set; } = FieldState.Empty;
+        public Agent Agent { get; set; }
+        public Stack<Piece> Pieces { get; set; }
+
+        public void Clean()
+        {
+            State = FieldState.Empty;
+            Agent = null;
+            Pieces.Clear();
+        }
+    }
+}
---
+++ TheProjectGame/GameMaster/Piece.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameMaster
+{
+    public class Piece
+    {
+        public bool IsSham { get; set; }
+    }
+}
