d1fb0baaa8a122537fa16d53a7ede10f27a816a2
Mateusz Śliwakowski <mateusz_sliw@interia.pl>
2020-03-16 17:28:14 +0100

    Add basic enums #IO-22

---
+++ TheProjectGame/GameMaster/Enums/FieldState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameMaster
+{
+    public enum FieldState
+    {
+        Empty,
+        Goal,
+        FakeGoal
+    }
+}
