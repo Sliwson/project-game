c0a335229da00f596c83fd0df1749a106f063498
Mateusz Śliwakowski <mateusz_sliw@interia.pl>
2020-03-16 17:28:56 +0100

    Sketch out game master class #IO-22

---
+++ TheProjectGame/GameMaster/GameMaster.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+
+namespace GameMaster
+{
+    public class GameMaster
+    {
+        private GameMasterState state = GameMasterState.Configuration;
+        private List<Agent> agents = new List<Agent>();
+        private BoardLogicComponent boardLogicComponent;
+
+        public GameMaster()
+        {
+            LoadDefaultConfiguration();
+
+            //create board with deafult parameters
+            boardLogicComponent = new BoardLogicComponent(new Point(5, 10));
+
+            //try to connect to communciation server
+        }
+
+        public void SetNetworkConfiguration(/*network configuration*/) { }
+        public void SetBoardConfiguration(/*board configuration*/) { }
+        public void SetAgentsConfiguartion(/*agents configuration*/) { }
+
+        public void ApplyConfiguration()
+        {
+            //if ok start accepting agents
+            state = GameMasterState.ConnectingAgents;
+        }
+
+        public void StartGame()
+        {
+            state = GameMasterState.InGame;
+        }
+
+        public void PauseGame()
+        {
+            state = GameMasterState.Paused;
+        }
+
+        //called from window system each frame, updates all components
+        public void Update()
+        {
+
+        }
+
+        private void LoadDefaultConfiguration()
+        {
+            
+        }
+    }
+}
