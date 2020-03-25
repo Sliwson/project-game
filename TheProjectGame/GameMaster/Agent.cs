82cb4f4dabc824d6f8028ff9244dff36f8052003
Mateusz Śliwakowski <mateusz_sliw@interia.pl>
2020-03-25 15:36:21 +0100

    Implement simple Agent class #IO-23

--- TheProjectGame/GameMaster/Agent.cs
+++ TheProjectGame/GameMaster/Agent.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Messaging.Enumerators;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,39 @@ namespace GameMaster
 {
     public class Agent
     {
+        public int Id { get; private set; }
+        public TeamId Team { get; private set; }
+        public bool IsTeamLeader { get; private set; } = false;
+        public double Timeout { get; private set; } = 0;
+        public Piece Piece { get; private set; } = null;
+
+        public Agent(int id, TeamId team, bool isTeamLeader = false)
+        {
+            Id = id;
+            Team = team;
+            IsTeamLeader = isTeamLeader;
+        }
+
+        public void Update(double dt)
+        {
+            Timeout = Math.Max(Timeout - dt, 0.0);
+        }
+
+        public void PickUpPiece(Piece p)
+        {
+            Piece = p;
+        }
+
+        public Piece RemovePiece()
+        {
+            var p = Piece;
+            Piece = null;
+            return p;
+        }
+
+        public bool CanPerformAction()
+        {
+            return Timeout <= 0;
+        }
     }
 }
