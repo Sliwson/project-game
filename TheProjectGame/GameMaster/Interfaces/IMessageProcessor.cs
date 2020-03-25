737a23dbc14023c0f185014dbdd987c7bdd4879e
Mateusz Śliwakowski <mateusz_sliw@interia.pl>
2020-03-25 17:00:30 +0100

    Add IMessageProcessor interface #IO-23

---
+++ TheProjectGame/GameMaster/Interfaces/IMessageProcessor.cs
@@ -0,0 +1,12 @@
+﻿using Messaging.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameMaster.Interfaces
+{
+    interface IMessageProcessor
+    {
+        public BaseMessage ProcessMessage(BaseMessage message);
+    }
+}
