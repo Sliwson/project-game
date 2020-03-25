0b1e1ddf6115d49c32f54f341def7d9210955d47
Mateusz Śliwakowski <mateusz_sliw@interia.pl>
2020-03-25 17:01:05 +0100

    Add empty components #IO-23

---
+++ TheProjectGame/GameMaster/ConnectionLogicComponent.cs
@@ -0,0 +1,29 @@
+﻿using Messaging.Contracts;
+using Messaging.Contracts.Agent;
+using Messaging.Contracts.Errors;
+using Messaging.Implementation;
+using Messaging.Enumerators;
+using Messaging.Contracts.GameMaster;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using GameMaster.Interfaces;
+
+namespace GameMaster
+{
+    class ConnectionLogicComponent : IMessageProcessor
+    {
+        GameMaster gameMaster;
+
+        public ConnectionLogicComponent(GameMaster gameMaster)
+        {
+            this.gameMaster = gameMaster;
+        }
+
+        public BaseMessage ProcessMessage(BaseMessage message)
+        {
+            return null;
+        }
+
+    }
+}
---
+++ TheProjectGame/GameMaster/ScoreComponent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameMaster
+{
+    public class ScoreComponent
+    {
+    }
+}
