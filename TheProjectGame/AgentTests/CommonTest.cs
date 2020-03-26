9c20608bd1915cbca5d5117344c2a6d8f1adfc03
Marta Banel <01130552@pw.edu.pl>
2020-03-26 15:49:06 +0100

    Add draft for common agent tests #IO-16

---
+++ TheProjectGame/AgentTests/CommonTest.cs
@@ -0,0 +1,21 @@
+﻿using Agent;
+using Messaging.Enumerators;
+using NUnit.Framework;
+
+namespace AgentTests
+{
+    public class CommonTest
+    {
+        [Test]
+        public void GetGoalDirectionDoesNotReturnOppositeDirection()
+        {
+            //int shortTime = 4;
+            //Agent.Agent agentRed = new Agent.Agent();
+            //agentRed.Initialize(..., team.Red, new Point(2, 2), new Point(3, 3), new int[3, 3]);
+            //Assert.AreNotEqual(Common.GetGoalDirection(agentRed, shortTime), Direction.South);
+            //Agent.Agent agentBlue = new Agent.Agent();
+            //agentBlue.Initialize(..., team.Blue, new Point(2, 2), new Point(3, 3), new int[3, 3]);
+            //Assert.AreNotEqual(Common.GetGoalDirection(agentBlue, shortTime), Direction.North);
+        }
+    }
+}
