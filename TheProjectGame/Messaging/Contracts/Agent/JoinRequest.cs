95add637f03360bfa3b116ddd6e3ceb4888e99bd
Mateusz Śliwakowski <mateusz_sliw@interia.pl>
2020-03-26 19:18:11 +0100

    Implement ConnectionLogic #IO-23

--- TheProjectGame/Messaging/Contracts/Agent/JoinRequest.cs
+++ TheProjectGame/Messaging/Contracts/Agent/JoinRequest.cs
@@ -7,10 +7,12 @@ namespace Messaging.Contracts.Agent
         public MessageId GetMessageId() => MessageId.JoinRequest;
 
         public TeamId TeamId { get; private set; }
+        public bool IsTeamLeader { get; private set; }
 
-        public JoinRequest(TeamId teamId)
+        public JoinRequest(TeamId teamId, bool isTeamLeader)
         {
             TeamId = teamId;
+            IsTeamLeader = isTeamLeader;
         }
     }
 }
