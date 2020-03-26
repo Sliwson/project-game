b99fe254b31b7a504f21a9bf5952595c7668922f
Szymon Stasiak <szymon-stasiak@outlook.com>
2020-03-26 19:19:19 +0100

    Add GameLogicTest class #IO-28

---
+++ TheProjectGame/GameMasterTests/GameLogicTest.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using GameMaster;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Messaging.Contracts;
+using Messaging.Contracts.GameMaster;
+using Messaging.Contracts.Agent;
+using Messaging.Implementation;
+using Messaging.Enumerators;
+
+namespace GameMasterTests
+{
+    public class GameLogicTest
+    {
+        private GameMaster.GameMaster gameMaster;
+        private GameLogicComponent gameLogicComponent;
+
+        [SetUp]
+        public void Setup()
+        {
+            gameMaster = new GameMaster.GameMaster();
+            gameLogicComponent = gameMaster.GameLogic;
+        }
+
+        [Test]
+        public void ProcessMessage_ShouldReturnUndefinedErrorMessageWhenAgentNotConnected()
+        {
+            var message = GetBaseMessage(new PutDownPieceRequest(), 666);
+
+            var response = gameLogicComponent.ProcessMessage(message);
+            Assert.AreEqual(MessageId.UndefinedError, response.MessageId);
+        }
+
+
+        // This method simulates normal situation where messages are stored in IEnumerable<BaseMessage>
+        private BaseMessage GetBaseMessage<T>(T payload, int agentFromId) where T:IPayload
+        {
+            return MessageFactory.GetMessage(payload, agentFromId);
+        }
+    }
+}
