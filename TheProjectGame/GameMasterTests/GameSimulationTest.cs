46887dea3bb7773e660e9b4a1e98c36fb865322f
Mateusz Śliwakowski <mateusz_sliw@interia.pl>
2020-03-27 12:01:55 +0100

    Implement simple game loop test #IO-30

--- TheProjectGame/GameMasterTests/GameSimulationTest.cs
+++ TheProjectGame/GameMasterTests/GameSimulationTest.cs
@@ -18,6 +18,7 @@ namespace GameMasterTests
     {
         private GameMaster.GameMaster gameMaster;
         private GameMasterConfiguration config;
+        private Random random = new Random();
 
         [SetUp]
         public void Setup()
@@ -30,7 +31,8 @@ namespace GameMasterTests
         public void Simulation()
         {
             gameMaster.ApplyConfiguration();
-            CreateAndConnectAgents(); 
+            CreateAndConnectAgents();
+            Play();
         }
 
         private void CreateAndConnectAgents()
@@ -39,13 +41,11 @@ namespace GameMasterTests
             var agentMessages = new List<BaseMessage>();
 
             for (int i = 0; i < limit * 2; i++)
-                agentMessages.Add(MessageFactory.GetMessage(new JoinRequest(
+                gameMaster.InjectMessage(MessageFactory.GetMessage(new JoinRequest(
                     i < limit ? TeamId.Blue : TeamId.Red,
                     i % limit == 0
                     ), i));
 
-            foreach (var m in agentMessages)
-                gameMaster.InjectMessage(m);
 
             gameMaster.Update(0);
             gameMaster.StartGame();
@@ -54,9 +54,44 @@ namespace GameMasterTests
             Assert.AreEqual(2 * limit, agents.Count);
             Assert.AreEqual(2, agents.Where(a => a.IsTeamLeader == true).Count());
             Assert.AreEqual(limit, agents.Where(a => a.Team == TeamId.Blue).Count());
-
             for (int i = 0; i < 2 * limit; i++)
                 Assert.AreEqual(i, agents[i].Id);
+        }
+
+        private void Play()
+        {
+            var agents = gameMaster.Agents;
+            bool[] hasMoved = new bool[agents.Count];
+            Point[] previousPositions = new Point[agents.Count];
+
+            //simulate 100 move requests for each agent
+            for (int i = 0; i < 100; i++)
+            {
+                for (int j = 0; j < agents.Count; j++)
+                    previousPositions[j] = agents[j].Position;
+
+                foreach (var a in agents)
+                    gameMaster.InjectMessage(CreateRandomMoveMessage(a));
+
+                gameMaster.Update(1.0);
+
+                for (int j = 0; j < agents.Count; j++)
+                    if (previousPositions[j] != agents[j].Position)
+                        hasMoved[j] = true;
+            }
+
+            Assert.IsTrue(hasMoved.All(b => b == true));
+        }
+
+        private BaseMessage CreateRandomMoveMessage(Agent agent)
+        {
+            return MessageFactory.GetMessage(new MoveRequest(GetRandomDirection()), agent.Id);
+        }
+
+        private Direction GetRandomDirection()
+        {
+            int number = random.Next(0, 4);
+            return (Direction)number;
         }
     }
 }
