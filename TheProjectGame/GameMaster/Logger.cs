61ae002673133ddb2e32b1ecbf6293bb29ef827c
Mateusz Śliwakowski <mateusz_sliw@interia.pl>
2020-04-04 11:07:41 +0200

    Prototype logger #IO-32

---
+++ TheProjectGame/GameMaster/Logger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NLog;
+
+namespace GameMaster
+{
+    public class GMLogger
+    {
+        private Logger logger =  LogManager.GetLogger("Logger");
+        private NLog.Targets.MemoryTarget memoryTarget;
+        private int logsRead = 0;
+
+        public GMLogger()
+        {
+            var config = new NLog.Config.LoggingConfiguration();
+
+            memoryTarget = new NLog.Targets.MemoryTarget();
+            memoryTarget.Layout = "${message}";
+            config.AddRuleForAllLevels(memoryTarget);
+            LogManager.Configuration = config;
+        }
+
+        public Logger Get()
+        {
+            return logger;
+        }
+
+        public List<string> GetPendingLogs()
+        {
+            var logs = new List<string>(memoryTarget.Logs);
+            logs = logs.TakeLast(logs.Count - logsRead).ToList();
+            logsRead = logs.Count;
+            return logs;
+        }
+    }
+}
