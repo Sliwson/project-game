323f428fa029918b6e8a2431fb48aaaa50e9a6d8
Michał Rogala <gm.rogala@gmail.com>
2020-04-11 13:04:49 +0200

    Refactor PresentationField

--- TheProjectGame/GameMaster/PresentationField.cs
+++ TheProjectGame/GameMaster/PresentationField.cs
@@ -1,20 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace GameMaster
+﻿namespace GameMaster
 {
     public class PresentationField
     {
-        public bool hasPiece { get; private set; }
-        public bool isSham { get; private set; }
+        public bool HasPiece { get; private set; }
+        public bool IsSham { get; private set; }
         public FieldState State { get; private set; }
 
         public PresentationField(bool hasPiece, bool isSham, FieldState State)
         {
-            this.hasPiece = hasPiece;
-            this.isSham = isSham;
+            this.HasPiece = hasPiece;
+            this.IsSham = isSham;
             this.State = State;
+        }
+
+        public PresentationField(Field field)
+        {
+            if (field.State == FieldState.Empty)
+            {
+                HasPiece = field.Pieces.Count != 0;
+                IsSham = false;
+                if (HasPiece)
+                {
+                    IsSham = field.Pieces.Peek().IsSham;
+                }
+            }
+            else
+            {
+                HasPiece = false;
+                IsSham = false;
+                State = field.State;
+            }
         }
     }
 }
