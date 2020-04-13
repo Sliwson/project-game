d43c1dbf103bd8db726758b8a4a8a1934e3806ee
Szymon Stasiak <szymon-stasiak@outlook.com>
2020-04-13 23:14:45 +0200

    #IO-26 Add Newtonsoft.Json to Messaging, create MessageSerializer class

---
+++ TheProjectGame/Messaging/Serialization/MessageSerializer.cs
@@ -0,0 +1,18 @@
+﻿using Messaging.Contracts;
+
+namespace Messaging.Serialization
+{
+    public static class MessageSerializer
+    {
+        public static string SerializeMessage(BaseMessage message)
+        {
+            return null;
+        }
+
+        public static BaseMessage DeserializeMessage(string serializedMessage)
+        {
+            return null;
+        }
+
+    }
+}
