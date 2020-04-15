3ddf687fed111c91cd306c6482ad0d5881f0baf6
Mateusz Śliwakowski <mateusz_sliw@interia.pl>
2020-04-15 09:31:00 +0200

    Add ExchangeInformationMessagePayload to messages #IO-49

---
+++ TheProjectGame/Messaging/Contracts/GameMaster/ExchangeInformationResponsePayload.cs
@@ -0,0 +1,46 @@
+﻿using Messaging.Contracts.Agent;
+using Messaging.Enumerators;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Messaging.Contracts.GameMaster
+{
+    class ExchangeInformationResponsePayload : IPayload
+    {
+        public MessageId GetMessageId() => MessageId.ExchangeInformationResponseMessage;
+        
+        [JsonRequired]
+        [JsonProperty(PropertyName = "respondingId")]
+        public int RespondingId { get; private set; }
+
+        // TODO: Make sure how to format those arrays into one-dimensional
+        [JsonRequired]
+        [JsonProperty(PropertyName = "distances")]
+        public int[,] Distances { get; private set; }
+
+        [JsonRequired]
+        [JsonProperty(PropertyName = "redTeamGoalAreaInformations")]
+        public GoalInformation[,] RedTeamGoalAreaInformation { get; private set; }
+
+        [JsonRequired]
+        [JsonProperty(PropertyName = "blueTeamGoalAreaInformations")]
+        public GoalInformation[,] BlueTeamGoalAreaInformation { get; private set; }
+
+        public ExchangeInformationResponsePayload(int respondingId, int[,] distances, GoalInformation[,] redTeamGoalAreaInformation, GoalInformation[,] blueTeamGoalAreaInformation)
+        {
+            RespondingId = respondingId;
+            Distances = distances;
+            RedTeamGoalAreaInformation = redTeamGoalAreaInformation;
+            BlueTeamGoalAreaInformation = blueTeamGoalAreaInformation;
+        }
+
+        public ExchangeInformationResponsePayload(ExchangeInformationResponse message) : this (
+            message.RespondToId,
+            message.Distances,
+            message.RedTeamGoalAreaInformation,
+            message.BlueTeamGoalAreaInformation
+        ) { }
+    }
+}
--- TheProjectGame/Messaging/Enumerators/MessageId.cs
+++ TheProjectGame/Messaging/Enumerators/MessageId.cs
@@ -9,34 +9,35 @@ namespace Messaging.Enumerators
     public enum MessageId
     {
         // Agent's Messages
-        CheckShamRequest =              001,
-        DestroyPieceRequest =           002,
-        DiscoverRequest =               003,
-        ExchangeInformationResponse =   004,
-        ExchangeInformationRequest =    005,
-        JoinRequest =                   006,
-        MoveRequest =                   007,
-        PickUpPieceRequest =            008,
-        PutDownPieceRequest =           009,
+        CheckShamRequest =                   001,
+        DestroyPieceRequest =                002,
+        DiscoverRequest =                    003,
+        ExchangeInformationResponse =        004,
+        ExchangeInformationRequest =         005,
+        JoinRequest =                        006,
+        MoveRequest =                        007,
+        PickUpPieceRequest =                 008,
+        PutDownPieceRequest =                009,
 
         // Game Master's Messages:
-        CheckShamResponse =             101,
-        DestroyPieceResponse =          102,
-        DiscoverResponse =              103,
-        EndGameMessage =                104,
-        StartGameMessage =              105,
-        ExchangeInformationMessage =    106,
-        JoinResponse =                  107,
-        MoveResponse =                  108,
-        PickUpPieceResponse =           109,
-        PutDownPieceResponse =          110,
+        CheckShamResponse =                  101,
+        DestroyPieceResponse =               102,
+        DiscoverResponse =                   103,
+        EndGameMessage =                     104,
+        StartGameMessage =                   105,
+        ExchangeInformationMessage =         106,
+        JoinResponse =                       107,
+        MoveResponse =                       108,
+        PickUpPieceResponse =                109,
+        PutDownPieceResponse =               110,
+        ExchangeInformationResponseMessage = 111,
 
         // Error Messages:
-        MoveError =                     901,
-        PickUpPieceError =              902,
-        PutDownPieceError =             903,
-        IgnoredDelayError =             904,
-        UndefinedError =                905
+        MoveError =                          901,
+        PickUpPieceError =                   902,
+        PutDownPieceError =                  903,
+        IgnoredDelayError =                  904,
+        UndefinedError =                     905
     }
 
     public static class MessageIdExtensions
