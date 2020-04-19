2f016f536e08af82bf583edd2b2fb7cecf168d66
Michał Rogala <gm.rogala@gmail.com>
2020-04-19 15:29:03 +0200

    Bind current configuration with label and button availability

---
+++ TheProjectGame/GameMasterPresentation/GMConfigToBoolConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace GameMasterPresentation
+{
+    public class GMConfigToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Configuration.Configuration conf)
+            {
+                if (conf != null)
+                {
+                    return conf.Validate();
+                }
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
