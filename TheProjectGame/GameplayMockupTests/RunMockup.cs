8e1b227a91a30c3d13fc870f3d9f014a6ce840f9
Mateusz Śliwakowski <mateusz_sliw@interia.pl>
2020-04-20 14:04:03 +0200

    Create application object #IO-58

--- TheProjectGame/GameplayMockupTests/RunMockup.cs
+++ TheProjectGame/GameplayMockupTests/RunMockup.cs
@@ -1,6 +1,7 @@
 using GameMasterPresentation;
 using NUnit.Framework;
 using System.Threading;
+using System.Windows;
 
 namespace GameplayMockupTests
 {
@@ -16,6 +17,7 @@ namespace GameplayMockupTests
         {
             var gmThread = new Thread(() =>
             {
+                new App(); //this is magic, but absolutely neceessary because it sets Application.Current
                 var window = new MainWindow();
                 window.Closed += (s, e) => window.Dispatcher.InvokeShutdown();
                 window.Show();
