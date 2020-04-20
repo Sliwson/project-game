49bcea93e6dad60bfaae3900bec52777388608e5
Mateusz Śliwakowski <mateusz_sliw@interia.pl>
2020-04-20 17:33:27 +0200

    Disable Info and Trace logging #IO-58

--- TheProjectGame/GameMaster/Logger.cs
+++ TheProjectGame/GameMaster/Logger.cs
@@ -24,8 +24,11 @@ namespace GameMaster
             fileTarget.FileName = "gm-log.txt";
             fileTarget.Layout = layout;
 
-            config.AddRuleForAllLevels(memoryTarget);
-            config.AddRuleForAllLevels(fileTarget);
+            config.AddRuleForOneLevel(LogLevel.Error, memoryTarget);
+            config.AddRuleForOneLevel(LogLevel.Warn, memoryTarget);
+
+            //config.AddRuleForAllLevels(memoryTarget);
+            //config.AddRuleForAllLevels(fileTarget);
 
             LogManager.Configuration = config;
         }
