12d1b19f4e25b3d69341b1e26e2a9cde5eabd2eb
Mateusz Śliwakowski <mateusz_sliw@interia.pl>
2020-04-20 14:36:06 +0200

    Add agents threads#IO-58

--- TheProjectGame/GameplayMockupTests/RunMockup.cs
+++ TheProjectGame/GameplayMockupTests/RunMockup.cs
@@ -1,6 +1,9 @@
+using Agent;
 using CommunicationServer;
 using GameMasterPresentation;
 using NUnit.Framework;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading;
 using System.Windows;
 
@@ -9,15 +12,13 @@ namespace GameplayMockupTests
     public class Tests
     {
         private static string csConfigFilePath = @"communicationServerConfig.json";
-
-        [SetUp]
-        public void Setup()
-        {
-        }
+        const int agentsInTeam = 1;
+        const int agentSleepMs = 16;
 
         [Test]
         public void RunMockup()
         {
+            //configure gm thread
             var gmThread = new Thread(() =>
             {
                 new App(); //this is magic, but absolutely neceessary because it sets Application.Current
@@ -27,18 +28,53 @@ namespace GameplayMockupTests
 
                 System.Windows.Threading.Dispatcher.Run();
             });
-
             gmThread.SetApartmentState(ApartmentState.STA);
 
-
+            //configure cs thread
             var csThread = new Thread(() =>
             {
                 CommunicationServer.CommunicationServer server = new CommunicationServer.CommunicationServer(csConfigFilePath);
                 server.Run();
             });
 
+            var agentsThreads = new List<Thread>();
+            for (int i = 0; i < agentsInTeam * 2; i++)
+            {
+                agentsThreads.Add(new Thread(() => {
+                    var agent = new Agent.Agent(new AgentConfiguration {
+                        CsIP = "127.0.0.1",
+                        CsPort = 54321,
+                        TeamID = i < agentsInTeam ? "red" : "blue",
+                        WantsToBeTeamLeader = i % agentsInTeam == 0
+                    });
+
+                    agent.ConnectToCommunicationServer();
+
+                    Stopwatch stopwatch = new Stopwatch();
+                    stopwatch.Start();
+
+                    ActionResult actionResult = ActionResult.Continue;
+                    while (actionResult == ActionResult.Continue)
+                    {
+                        stopwatch.Stop();
+                        var timeElapsed = stopwatch.Elapsed.TotalSeconds;
+                        stopwatch.Reset();
+                        stopwatch.Start();
+
+                        actionResult = agent.Update(timeElapsed);
+                        Thread.Sleep(agentSleepMs);
+                    }
+
+                    agent.OnDestroy();
+                }));
+            }
+
             csThread.Start();
             gmThread.Start();
+
+            Thread.Sleep(3000);
+            foreach (var t in agentsThreads)
+                t.Start();
 
             gmThread.Join();
         }
