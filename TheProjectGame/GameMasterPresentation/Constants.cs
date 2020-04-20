03e8c817839905a1631b66f46ddd72d7323968e3
Mateusz Śliwakowski <mateusz_sliw@interia.pl>
2020-04-20 13:45:53 +0200

    Add configuration copying to bin folder #IO-58

--- TheProjectGame/GameMasterPresentation/Constants.cs
+++ TheProjectGame/GameMasterPresentation/Constants.cs
@@ -8,8 +8,8 @@ namespace GameMasterPresentation
     public static class Constants
     {
         public static readonly string MessageBoxName = "Configuration";
-        public static readonly string ConfigurationDirectoryPath = "..\\..\\..\\Configuration";
-        public static readonly string ConfigurationFilePath = "..\\..\\..\\Configuration\\GameMasterConfiguration.json";
+        public static readonly string ConfigurationDirectoryPath = @"Configuration";
+        public static readonly string ConfigurationFilePath = @"Configuration\GameMasterConfiguration.json";
 
         public static readonly int HorizontalLineZIndex = 50;
         public static readonly int VerticalLineZIndex = 50;
