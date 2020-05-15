a47f1a8747dc0809ebfb9578a23117fb78c62c1a
Michał Rogala <gm.rogala@gmail.com>
2020-05-16 01:33:27 +0200

    Add colors to logs

--- TheProjectGame/GameMasterPresentation/LogEntry.cs
+++ TheProjectGame/GameMasterPresentation/LogEntry.cs
@@ -1,16 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
-using System.Text;
+using System.Windows.Media;
 
 namespace GameMasterPresentation
 {
     public class LogEntry : INotifyPropertyChanged
     {
         private string _Message;
-        public string Message 
-        { 
+
+        public string Message
+        {
             get
             {
                 return _Message;
@@ -18,6 +19,21 @@ namespace GameMasterPresentation
             set
             {
                 _Message = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private SolidColorBrush _color;
+
+        public SolidColorBrush Color
+        {
+            get
+            {
+                return _color;
+            }
+            set
+            {
+                _color = value;
                 NotifyPropertyChanged();
             }
         }
@@ -32,6 +48,54 @@ namespace GameMasterPresentation
         public LogEntry(string text)
         {
             Message = text;
+            var trimmedText = Message.Trim();
+            var substr = trimmedText.Substring(15, 4);
+            switch(substr)
+            {
+                case "Warn":
+                    Color = new SolidColorBrush(Colors.Orange);
+                    return;
+                case "Erro":
+                    Color = new SolidColorBrush(Colors.Red);
+                    return;
+                default:
+                    Color = new SolidColorBrush(Colors.Black);
+                    break;
+            }
+
+            substr = trimmedText.Substring(trimmedText.IndexOf('-') + 1);
+            substr = substr.Trim();
+            substr = substr.Substring(0, 4);
+
+            switch(substr)
+            {
+                case "[GM]":
+                    Color = new SolidColorBrush(Colors.Black);
+                    break;
+                case "[CS]":
+                    Color = new SolidColorBrush(Colors.Purple);
+                    break;
+                case "[Con"://Connection
+                    Color = new SolidColorBrush(Colors.DodgerBlue);
+                    break;
+                case "[Age"://Agent
+                    Color = new SolidColorBrush(Colors.LightSlateGray);
+                    break;
+                case "[Hos"://HostMapping
+                    Color = new SolidColorBrush(Colors.DarkMagenta);
+                    break;
+                case "[Net"://NetworkComponent
+                    Color = new SolidColorBrush(Colors.LightSkyBlue);
+                    break;
+                case "[Boa"://Board
+                    Color = new SolidColorBrush(Colors.Navy);
+                    break;
+                case "[Sco"://Score
+                    Color = new SolidColorBrush(Colors.ForestGreen);
+                    break;
+            }
+
+
         }
     }
 }
