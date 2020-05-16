f3ff69621015ef73226125414c30a65fd8dfe330
Szymon Stasiak <szymon-stasiak@outlook.com>
2020-05-16 19:50:49 +0200

    #IO-50 Fix paths to schema files in MessagingTests

--- TheProjectGame/MessagingTests/MessageCompareToJsonSchemaTests.cs
+++ TheProjectGame/MessagingTests/MessageCompareToJsonSchemaTests.cs
@@ -16,6 +16,7 @@ namespace MessagingTests
     class MessageCompareToJsonSchemaTests
     {
         List<BaseMessage> messages;
+        string basePath = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\Schema\\"));
 
         [SetUp]
         public void SetUp()
@@ -35,107 +36,107 @@ namespace MessagingTests
                     // Agent
                     case MessageId.CheckShamRequest:
                         serialized = JsonConvert.SerializeObject((message as Message<CheckShamRequest>).Payload);
-                        schema = File.ReadAllText(@"..\..\..\Schema\Agent\checkForScham.json");
+                        schema = File.ReadAllText(Path.Combine(basePath, @"Agent\checkForScham.json"));
                         break;
                     case MessageId.DestroyPieceRequest:
                         serialized = JsonConvert.SerializeObject((message as Message<DestroyPieceRequest>).Payload);
-                        schema = File.ReadAllText(@"..\..\..\Schema\Agent\destroyPiece.json");
+                        schema = File.ReadAllText(Path.Combine(basePath, @"Agent\destroyPiece.json"));
                         break;
                     case MessageId.DiscoverRequest:
                         serialized = JsonConvert.SerializeObject((message as Message<DiscoverRequest>).Payload);
-                        schema = File.ReadAllText(@"..\..\..\Schema\Agent\discovery.json");
+                        schema = File.ReadAllText(Path.Combine(basePath, @"Agent\discovery.json"));
                         break;
                     case MessageId.ExchangeInformationResponse:
                         serialized = JsonConvert.SerializeObject((message as Message<ExchangeInformationResponse>).Payload);
-                        schema = File.ReadAllText(@"..\..\..\Schema\Agent\informationExchangeRespond.json");
+                        schema = File.ReadAllText(Path.Combine(basePath, @"Agent\informationExchangeRespond.json"));
                         break;
                     case MessageId.ExchangeInformationRequest:
                         serialized = JsonConvert.SerializeObject((message as Message<ExchangeInformationRequest>).Payload);
-                        schema = File.ReadAllText(@"..\..\..\Schema\Agent\informationExchangeAsk.json");
+                        schema = File.ReadAllText(Path.Combine(basePath, @"Agent\informationExchangeAsk.json"));
                         break;
                     case MessageId.JoinRequest:
                         serialized = JsonConvert.SerializeObject((message as Message<JoinRequest>).Payload);
-                        schema = File.ReadAllText(@"..\..\..\Schema\Agent\joinGame.json");
+                        schema = File.ReadAllText(Path.Combine(basePath, @"Agent\joinGame.json"));
                         break;
                     case MessageId.MoveRequest:
                         serialized = JsonConvert.SerializeObject((message as Message<MoveRequest>).Payload);
-                        schema = File.ReadAllText(@"..\..\..\Schema\Agent\makeMove.json");
+                        schema = File.ReadAllText(Path.Combine(basePath, @"Agent\makeMove.json"));
                         break;
                     case MessageId.PickUpPieceRequest:
                         serialized = JsonConvert.SerializeObject((message as Message<PickUpPieceRequest>).Payload);
-                        schema = File.ReadAllText(@"..\..\..\Schema\Agent\pickPiece.json");
+                        schema = File.ReadAllText(Path.Combine(basePath, @"Agent\pickPiece.json"));
                         break;
                     case MessageId.PutDownPieceRequest:
                         serialized = JsonConvert.SerializeObject((message as Message<PutDownPieceRequest>).Payload);
-                        schema = File.ReadAllText(@"..\..\..\Schema\Agent\putPiece.json");
+                        schema = File.ReadAllText(Path.Combine(basePath, @"Agent\putPiece.json"));
                         break;
 
                     // GameMaster
                     case MessageId.CheckShamResponse:
                         serialized = JsonConvert.SerializeObject((message as Message<CheckShamResponse>).Payload);
-                        schema = File.ReadAllText(@"..\..\..\Schema\GameMaster\checkForSchamResponse.json");
+                        schema = File.ReadAllText(Path.Combine(basePath, @"GameMaster\checkForSchamResponse.json"));
                         break;
                     case MessageId.DestroyPieceResponse:
                         serialized = JsonConvert.SerializeObject((message as Message<DestroyPieceResponse>).Payload);
-                        schema = File.ReadAllText(@"..\..\..\Schema\GameMaster\destroyPieceResponse.json");
+                        schema = File.ReadAllText(Path.Combine(basePath, @"GameMaster\destroyPieceResponse.json"));
                         break;
                     case MessageId.DiscoverResponse:
                         serialized = JsonConvert.SerializeObject((message as Message<DiscoverResponse>).Payload);
-                        schema = File.ReadAllText(@"..\..\..\Schema\GameMaster\discoveryResponse.json");
+                        schema = File.ReadAllText(Path.Combine(basePath, @"GameMaster\discoveryResponse.json"));
                         break;
                     case MessageId.EndGameMessage:
                         serialized = JsonConvert.SerializeObject((message as Message<EndGamePayload>).Payload);
-                        schema = File.ReadAllText(@"..\..\..\Schema\GameMaster\gameEnd.json");
+                        schema = File.ReadAllText(Path.Combine(basePath, @"GameMaster\gameEnd.json"));
                         break;
                     case MessageId.ExchangeInformationRequestForward:
                         serialized = JsonConvert.SerializeObject((message as Message<ExchangeInformationRequestForward>).Payload);
-                        schema = File.ReadAllText(@"..\..\..\Schema\GameMaster\informationExchangeAskForward.json");
+                        schema = File.ReadAllText(Path.Combine(basePath, @"GameMaster\informationExchangeAskForward.json"));
                         break;
                     case MessageId.JoinResponse:
                         serialized = JsonConvert.SerializeObject((message as Message<JoinResponse>).Payload);
-                        schema = File.ReadAllText(@"..\..\..\Schema\GameMaster\joinGameResponse.json");
+                        schema = File.ReadAllText(Path.Combine(basePath, @"GameMaster\joinGameResponse.json"));
                         break;
                     case MessageId.MoveResponse:
                         serialized = JsonConvert.SerializeObject((message as Message<MoveResponse>).Payload);
-                        schema = File.ReadAllText(@"..\..\..\Schema\GameMaster\makeMoveResponse.json");
+                        schema = File.ReadAllText(Path.Combine(basePath, @"GameMaster\makeMoveResponse.json"));
                         break;
                     case MessageId.PickUpPieceResponse:
                         serialized = JsonConvert.SerializeObject((message as Message<PickUpPieceResponse>).Payload);
-                        schema = File.ReadAllText(@"..\..\..\Schema\GameMaster\pickPieceResponse.json");
+                        schema = File.ReadAllText(Path.Combine(basePath, @"GameMaster\pickPieceResponse.json"));
                         break;
                     case MessageId.PutDownPieceResponse:
                         serialized = JsonConvert.SerializeObject((message as Message<PutDownPieceResponse>).Payload);
-                        schema = File.ReadAllText(@"..\..\..\Schema\GameMaster\putPieceResponse.json");
+                        schema = File.ReadAllText(Path.Combine(basePath, @"GameMaster\putPieceResponse.json"));
                         break;
                     case MessageId.ExchangeInformationResponseForward:
                         serialized = JsonConvert.SerializeObject((message as Message<ExchangeInformationResponseForward>).Payload);
-                        schema = File.ReadAllText(@"..\..\..\Schema\GameMaster\informationExchangeRespondForward.json");
+                        schema = File.ReadAllText(Path.Combine(basePath, @"GameMaster\informationExchangeRespondForward.json"));
                         break;
                     case MessageId.StartGameMessage:
                         serialized = JsonConvert.SerializeObject((message as Message<StartGamePayload>).Payload);
-                        schema = File.ReadAllText(@"..\..\..\Schema\GameMaster\gameStart.json");
+                        schema = File.ReadAllText(Path.Combine(basePath, @"GameMaster\gameStart.json"));
                         break;
 
                     // Errors
                     case MessageId.MoveError:
                         serialized = JsonConvert.SerializeObject((message as Message<MoveError>).Payload);
-                        schema = File.ReadAllText(@"..\..\..\Schema\Errors\invalidMove.json");
+                        schema = File.ReadAllText(Path.Combine(basePath, @"Errors\invalidMove.json"));
                         break;
                     case MessageId.PickUpPieceError:
                         serialized = JsonConvert.SerializeObject((message as Message<PickUpPieceError>).Payload);
-                        schema = File.ReadAllText(@"..\..\..\Schema\Errors\pickPiece.json");
+                        schema = File.ReadAllText(Path.Combine(basePath, @"Errors\pickPiece.json"));
                         break;
                     case MessageId.PutDownPieceError:
                         serialized = JsonConvert.SerializeObject((message as Message<PutDownPieceError>).Payload);
-                        schema = File.ReadAllText(@"..\..\..\Schema\Errors\putPieceError.json");
+                        schema = File.ReadAllText(Path.Combine(basePath, @"Errors\putPieceError.json"));
                         break;
                     case MessageId.IgnoredDelayError:
                         serialized = JsonConvert.SerializeObject((message as Message<IgnoredDelayError>).Payload);
-                        schema = File.ReadAllText(@"..\..\..\Schema\Errors\timePenaltyError.json");
+                        schema = File.ReadAllText(Path.Combine(basePath, @"Errors\timePenaltyError.json"));
                         break;
                     case MessageId.UndefinedError:
                         serialized = JsonConvert.SerializeObject((message as Message<UndefinedError>).Payload);
-                        schema = File.ReadAllText(@"..\..\..\Schema\Errors\undefinedErrorMessage.json");
+                        schema = File.ReadAllText(Path.Combine(basePath, @"Errors\undefinedErrorMessage.json"));
                         break;
                 }
 
