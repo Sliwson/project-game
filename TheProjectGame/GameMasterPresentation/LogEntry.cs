f77fbdc46f4635c22cadfd41d1372790c9a6e7d7
Michał Rogala <gm.rogala@gmail.com>
2020-05-19 11:40:31 +0200

    Check if string is long enough to make substring

--- TheProjectGame/GameMasterPresentation/LogEntry.cs
+++ TheProjectGame/GameMasterPresentation/LogEntry.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.ComponentModel;
-using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Windows.Media;
 
@@ -49,53 +48,65 @@ namespace GameMasterPresentation
         {
             Message = text;
             var trimmedText = Message.Trim();
-            var substr = trimmedText.Substring(15, 4);
-            switch(substr)
+            string substr = "";
+            if (trimmedText.Length >= 19)
+                substr = trimmedText.Substring(15, 4);
+            switch (substr)
             {
                 case "Warn":
                     Color = new SolidColorBrush(Colors.Orange);
                     return;
+
                 case "Erro":
                     Color = new SolidColorBrush(Colors.Red);
                     return;
+
                 default:
                     Color = new SolidColorBrush(Colors.Black);
                     break;
             }
-
             substr = trimmedText.Substring(trimmedText.IndexOf('-') + 1);
             substr = substr.Trim();
-            substr = substr.Substring(0, 4);
-
-            switch(substr)
+            if (substr.Length >= 4)
+                substr = substr.Substring(0, 4);
+            switch (substr)
             {
                 case "[GM]":
                     Color = new SolidColorBrush(Colors.Black);
                     break;
+
                 case "[CS]":
                     Color = new SolidColorBrush(Colors.Purple);
                     break;
+
                 case "[Con"://Connection
                     Color = new SolidColorBrush(Colors.DodgerBlue);
                     break;
+
                 case "[Age"://Agent
                     Color = new SolidColorBrush(Colors.LightSlateGray);
                     break;
+
                 case "[Hos"://HostMapping
                     Color = new SolidColorBrush(Colors.DarkMagenta);
                     break;
+
                 case "[Net"://NetworkComponent
                     Color = new SolidColorBrush(Colors.LightSkyBlue);
                     break;
+
                 case "[Cli"://ClientNetworkComponent
                     Color = new SolidColorBrush(Colors.LightSkyBlue);
                     break;
+
                 case "[Boa"://Board
                     Color = new SolidColorBrush(Colors.Navy);
                     break;
+
                 case "[Sco"://Score
                     Color = new SolidColorBrush(Colors.ForestGreen);
                     break;
+
                 case "[Log"://Logic
                     Color = new SolidColorBrush(Colors.Teal);
                     break;
