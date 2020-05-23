b9c17404a67804edf0de2fc1b8238ab0f1d94378
Szymon Stasiak <szymon-stasiak@outlook.com>
2020-05-23 20:38:09 +0200

    #IO-57 Implement injecting messages in GM using Moq

--- TheProjectGame/GameMasterTests/GameSimulationTest.cs
+++ TheProjectGame/GameMasterTests/GameSimulationTest.cs
@@ -1,5 +1,4 @@
-﻿#if DEBUG
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using GameMaster;
 using System;
 using System.Collections.Generic;
@@ -12,41 +11,45 @@ using Messaging.Implementation;
 using Messaging.Enumerators;
 using System.Drawing;
 using System.Linq;
+using Moq;
+using Messaging.Communication;
+using System.Threading;
 
 namespace GameMasterTests
 {
     public class GameplaySimulationTest
     {
-        private GameMaster.GameMaster gameMaster;
-        private GameMasterConfiguration config;
         private Random random = new Random();
+
+        private GameMasterConfiguration config;
+        private GameMaster.GameMaster gameMaster;
+        private Mock<INetworkComponent> mockedNetworkComponent;
+
+        private bool isBatchSent = false;
 
         [SetUp]
         public void Setup()
         {
             config = GameMasterConfiguration.GetDefault();
             gameMaster = new GameMaster.GameMaster(config);
+
+            mockedNetworkComponent = new Mock<INetworkComponent>();
+            mockedNetworkComponent.Setup(m => m.Connect(It.IsAny<ClientType>())).Returns(true);
+            mockedNetworkComponent.Setup(m => m.GetIncomingMessages()).Returns( GetMockedMessages() );
         }
 
-        // TODO (#IO-57): Mock NetworkComponent
-        [Test, Ignore("Need offline NetworkComponent")]
+        [Test]
         public void SimpleSimulationTest_GameMasterShouldConnectAgentsAndProcessMoveRequestsAfterGameStart()
         {
-            gameMaster.ConnectToCommunicationServer();
+            gameMaster.ConnectToCommunicationServer(mockedNetworkComponent.Object);
             CreateAndConnectAgents();
             Play();
         }
 
         private void CreateAndConnectAgents()
         {
-            int limit = config.TeamSize;
             var agentMessages = new List<BaseMessage>();
-
-            for (int i = 0; i < limit * 2; i++)
-                gameMaster.InjectMessage(MessageFactory.GetMessage(new JoinRequest(
-                    i < limit ? TeamId.Blue : TeamId.Red
-                    ), i));
-
+            int limit = config.TeamSize;
 
             gameMaster.Update(0);
             gameMaster.StartGame();
@@ -71,17 +74,42 @@ namespace GameMasterTests
                 for (int j = 0; j < agents.Count; j++)
                     previousPositions[j] = agents[j].Position;
 
-                foreach (var a in agents)
-                    gameMaster.InjectMessage(CreateRandomMoveMessage(a));
-
                 gameMaster.Update(1.0);
 
                 for (int j = 0; j < agents.Count; j++)
                     if (previousPositions[j] != agents[j].Position)
                         hasMoved[j] = true;
+
+                isBatchSent = false;
             }
 
             Assert.IsTrue(hasMoved.All(b => b == true));
+        }
+
+        private IEnumerable<BaseMessage> GetMockedMessages()
+        {
+            int limit = config.TeamSize;
+            var agents = gameMaster.Agents;
+
+            if (gameMaster.state == GameMasterState.ConnectingAgents)
+            {
+                for (int i = 0; i < limit * 2; i++)
+                {
+                    yield return MessageFactory.GetMessage(new JoinRequest(i < limit ? TeamId.Blue : TeamId.Red), i);
+                }
+            }
+
+            else if (gameMaster.state == GameMasterState.InGame)
+            {
+                while(!isBatchSent)
+                {
+                    foreach (var a in agents)
+                    {
+                        yield return CreateRandomMoveMessage(a);
+                    }
+                    isBatchSent = true;
+                }
+            }
         }
 
         private BaseMessage CreateRandomMoveMessage(Agent agent)
@@ -96,5 +124,3 @@ namespace GameMasterTests
         }
     }
 }
-#endif
-
